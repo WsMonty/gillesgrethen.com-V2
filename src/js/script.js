0c45c3354310275df08fb79069d2521ba181c363
WsMonty <grethen.gilles@gmail.com>
2022-02-25 12:59:27 +0100

    Finish card functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,53 @@
 'use strict';
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
+if (module.hot) {
+  module.hot.accept();
+}
 
-const links = document.querySelector(
-  '.list_links--left'
-);
+// selecting global elements
+const links = document.querySelector('.list_links--left');
 const cards = document.querySelectorAll('.card');
+const overlay = document.querySelector('.overlay');
+const allCards = document.querySelectorAll('.card');
+const btnCloseCards = document.querySelector('.btn-card_close');
 
-links.addEventListener('click', (e) => {
-  if (e.target.classList.contains('list_item'))
-    return;
+// Helper functions
 
-  cards.forEach((card) =>
-    card.classList.add('hidden')
-  );
-  const curCard = document.querySelector(
-    `.card_${e.target.dataset.name}`
-  );
+const showElement = function (element) {
+  element.classList.remove('hidden');
+  element.setAttribute('data-state', 'active');
+};
+const hideElement = function (element) {
+  element.classList.add('hidden');
+  element.setAttribute('data-state', 'not-active');
+};
 
-  curCard.classList.remove('hidden');
-});
+// Cards functionality
 
-// Gallery (GG4tet)
+let activeCard;
+
+const showCards = function (e) {
+  if (e.target.classList.contains('list_item')) return;
+  // Hide all cards, so only 1 is active
+  cards.forEach((card) => hideElement(card));
+  activeCard = document.querySelector(`.card_${e.target.dataset.name}`);
+  // show current card
+  showElement(activeCard);
+  // show overlay
+  showElement(overlay);
+};
+
+const hideCards = function () {
+  hideElement(activeCard);
+  hideElement(overlay);
+};
+
+// Gallery (Slider in Quartet Card)
 const gallery = function () {
-  const btnLeft =
-    document.querySelector('.btn_left');
-  const btnRight =
-    document.querySelector('.btn_right');
-  const imgs = document.querySelectorAll(
-    '.img_quartet'
-  );
-  const dotContainer =
-    document.querySelector('.dots');
+  const btnLeft = document.querySelector('.btn_left');
+  const btnRight = document.querySelector('.btn_right');
+  const imgs = document.querySelectorAll('.img_quartet');
+  const dotContainer = document.querySelector('.dots');
 
   let curSlide = 0;
   const maxSlide = imgs.length;
@@ -50,23 +64,16 @@ const gallery = function () {
   const activateDot = function (slide) {
     document
       .querySelectorAll('.dots_dot')
-      .forEach((dot) =>
-        dot.classList.remove('dots_dot--active')
-      );
+      .forEach((dot) => dot.classList.remove('dots_dot--active'));
 
     document
-      .querySelector(
-        `.dots_dot[data-slide="${slide}"]`
-      )
+      .querySelector(`.dots_dot[data-slide="${slide}"]`)
       .classList.add('dots_dot--active');
   };
 
   const goToSlide = function (slide) {
     imgs.forEach(
-      (img, i) =>
-        (img.style.transform = `translateX(${
-          (i - slide) * 100
-        }%)`)
+      (img, i) => (img.style.transform = `translateX(${(i - slide) * 100}%)`)
     );
   };
 
@@ -98,25 +105,26 @@ const gallery = function () {
   btnRight.addEventListener('click', nextSlide);
   btnLeft.addEventListener('click', prevSlide);
 
-  document.addEventListener(
-    'keydown',
-    function (e) {
-      e.key === 'ArrowLeft' && prevSlide(); //like: if(e.key === 'Arrowleft') prevSlide;
-      e.key === 'ArrowRight' && nextSlide();
-    }
-  );
+  document.addEventListener('keydown', function (e) {
+    e.key === 'ArrowLeft' && prevSlide(); //like: if(e.key === 'Arrowleft') prevSlide;
+    e.key === 'ArrowRight' && nextSlide();
+  });
 
-  dotContainer.addEventListener(
-    'click',
-    function (e) {
-      if (
-        e.target.classList.contains('dots__dot')
-      ) {
-        const { slide } = e.target.dataset; //destructering, same as: const slide = e.target.dataset.slide
-        goToSlide(slide);
-        activateDot(slide);
-      }
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots_dot')) {
+      const { slide } = e.target.dataset; //destructering, same as: const slide = e.target.dataset.slide
+      goToSlide(slide);
+      activateDot(slide);
     }
-  );
+  });
 };
 gallery();
+
+// Event Hanlders
+
+links.addEventListener('click', (e) => showCards(e));
+overlay.addEventListener('click', hideCards);
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && overlay.dataset.state === 'active') hideCards();
+});
+btnCloseCards.addEventListener('click', hideCards);
